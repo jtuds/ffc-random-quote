93a532195565dd52da911ba7515fcc11421cc7f5
James Tudsbury <james.tudsbury@dontbeshy.com>
2019-01-10 18:46:08 +0000

    Various passing tests including checking the shape of the API returned value :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -3,10 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quoteText: 'placeholder quote',
+      quoteAuthor: 'placeholder author',
+    }
+
+    this.getRandomQuote = this.getRandomQuote.bind(this);
+    this.updateQuote = this.updateQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateQuote()
+  }
+
+  updateQuote() {
+    this.getRandomQuote()
+    .then(quoteObject => {
+      this.setState(() => ({
+        quoteText: quoteObject.quote,
+        quoteAuthor: quoteObject.author
+      }))
+    })
+  }
+
+  getRandomQuote() {
+    return fetch('http://quotes.stormconsultancy.co.uk/random.json') // returns a promise
+    .then(response => response.json())
+    .then(responseJson => responseJson)
+  }
+
   render() {
     return (
       <div id="quote-box" className="App">
         <header className="App-header">
+          <h1 id="text">{this.state.quoteText}</h1>
+          <p id="author">{this.state.quoteAuthor}</p>
+          <button id="new-quote" onClick={this.updateQuote}>new quote</button>
+          <button id="tweet-quote">tweet quote</button>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { doesNotReject } from 'assert';
+
+/**
+ * Using Jest for the test suite https://jestjs.io/docs/en/setup-teardown
+ * And Enzyme for DOM stuff and rendering React elements "https://github.com/airbnb/enzyme"
+ */
 
 configure({ adapter: new Adapter() });
 
@@ -21,3 +27,68 @@ it('has a wrapper ID of "quote-box"', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find('#quote-box').length).toEqual(1);
 });
+
+it('has a text element with an ID of "text', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#text').length).toEqual(1);
+})
+
+it('has an element with an ID of "author"', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#author').length).toEqual(1);
+})
+
+it('has an element with an ID of "new-quote"', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#new-quote').length).toEqual(1);
+})
+
+it('has a button element with an ID "new-quote"', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#new-quote').type()).toEqual('button');
+})
+
+it('has an element with an ID of "tweet-quote"', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#tweet-quote').length).toEqual(1);
+})
+
+it('has a button element with an ID "tweet-quote"', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#tweet-quote').type()).toEqual('button');
+})
+
+it('calls getRandomQuote on load', () => {
+  const spy = jest.spyOn(App.prototype, 'updateQuote');
+  shallow(<App />);
+  expect(spy).toHaveBeenCalled();
+})
+
+it('returns a quote object', () => {
+  const wrapper = shallow(<App />);
+  const getRandomQuote = wrapper.instance().getRandomQuote();
+  return getRandomQuote.then(result => expect(result).toEqual({
+    'author': expect.any(String),
+    'id': expect.any(Number),
+    'permalink': expect.any(String),
+    'quote': expect.any(String),
+  }));
+})
+
+// it('calls the quote API and updates the quote', () => {
+//   const wrapper = shallow(<App />);
+//   // expect(wrapper.instance().updateQuote()).objectContaining({
+//   //   'quote': expect.any(String),
+//   //   'author': expect.any(String),
+//   // })
+//   // https://github.com/mpj/unit-testing-series/tree/c876ef875cf05deecd65b59600118d474f45c521
+//   // here is where I test getRandomQuote similar to have MPJ tested orderTotal
+//   // I want to call it here and then assert what it returns
+//   // If i call it with mocked out params, perhaps the URL and fetch, does it return what I expect
+//   // the expected return is that it calls updateQuote with the correct shape of data
+//   // after that I can test updateQuote itself
+
+//   // getRandomQuote(url, fetch).expect(returnedvalue).toBe(a call to updateQuote)
+
+//   //maybe getRandomQuote should return an object and updateQuote should call getRandomQuote otherwise getRandomQuote has some side effects
+// })
